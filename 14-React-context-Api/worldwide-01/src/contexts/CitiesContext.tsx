abb77e189f048f3b1a29128fcf07cbeca79b18ed
AliBourara <bourara.ali.pro@gmail.fr>
2024-03-15 11:58:58 +0100

    Created the Part One useContext Section

---
+++ 14-React-context-Api/worldwide-01/src/contexts/CitiesContext.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { Citiestype } from "../../data/data";
+import { BASE_URL } from "../App";
+
+interface CitiesProviderProp {
+  children: ReactNode;
+}
+
+interface CitiesContexttype {
+  cities: Citiestype[] | null;
+  setCities?: React.Dispatch<React.SetStateAction<Citiestype[]>>;
+  isLoading: boolean;
+  setIsLoading?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CitiesContext = createContext<CitiesContexttype>({} as CitiesContexttype);
+
+function CitiesProvider({ children }: CitiesProviderProp) {
+  const [cities, setCities] = useState<Citiestype[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+export { CitiesProvider };
